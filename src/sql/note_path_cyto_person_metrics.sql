6b626862a4df6b112277390a4815e76118047031
solomonahenry <shenry@stanford.edu>
2025-02-07 22:15:12 +0000

    added SQL scripts

---
+++ src/sql/note_path_cyto_person_metrics.sql
@@ -0,0 +1,10 @@
+with
+person as (select * from `som-rit-phi-oncology-prod.oncology_omop_arpah_alpha.person`),
+note as (select * from `som-rit-phi-oncology-prod.oncology_omop_arpah_alpha.note`)
+select
+count(distinct person.person_source_value) patient_count
+from
+note
+join person on note.person_id = person.person_id
+where
+note_title in ('pathology','pathology and cytology')
