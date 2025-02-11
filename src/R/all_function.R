8866a23369cbdd1b7c142bd67b2db1fb23b26d7f
Farnoosh-Starr <farnoosh@stanford.edu>
2025-02-11 19:49:13 +0000

    added functions

---
+++ src/R/all_function.R
@@ -0,0 +1,74 @@
+############################
+### connection function ###
+############################
+library(DBI)
+library(bigrquery)
+library(readr)
+library(stringr)
+
+# Function to execute a BigQuery query and return the result as a DataFrame
+run_bigquery_query <- function(project_id, sql_file_path, credentials_path) {
+  # Set Google Cloud credentials
+  Sys.setenv(GOOGLE_APPLICATION_CREDENTIALS = credentials_path)
+  
+  # Authenticate with BigQuery
+  bq_auth(path = Sys.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+  
+  # Create BigQuery connection
+  bq_conn <- dbConnect(
+    bigrquery::bigquery(),
+    project = project_id,
+    use_legacy_sql = FALSE
+  )
+  
+  # Load SQL from the specified file
+  sql_query <- read_file(sql_file_path)
+  sql_query <- str_c(sql_query, collapse = " ") ## for any line break
+
+  # Execute the query and return results
+  result_df <- tryCatch({
+    query_result <- dbGetQuery(bq_conn, sql_query)
+    print("Query executed successfully!")
+    return(query_result)
+  }, error = function(e) {
+    print(paste("Query failed:", e$message))
+    return(NULL)
+  })
+  
+  # Disconnect from BigQuery
+  dbDisconnect(bq_conn)
+  
+  return(result_df)
+}
+
+#######################
+### plot function ###
+#######################
+create_bar_plot <- function(data, x_var, y_var, plot_title, x_axis_title, y_axis_title) {
+  plot_ly(
+    data = data, 
+    x = data[[x_var]], 
+    y = data[[y_var]], 
+    type = 'bar', 
+    marker = list(color = 'rgba(104, 198, 214, 0.7)', line = list(color = 'rgb(100, 173, 237)', width = 1.5))
+  ) %>%
+    layout(
+       title = list(
+        text = plot_title,
+        x = 0.01,  # Align to the left (0 = far left, 1 = far right)
+        xanchor = "left"
+      ),
+      xaxis = list(
+        title = x_axis_title,
+        tickangle = -45,
+        showgrid = FALSE
+      ),
+      yaxis = list(
+        title = y_axis_title,
+        rangemode = "tozero"
+      ),
+      margin = list(b = 100),
+      showlegend = FALSE
+    )
+}
+
