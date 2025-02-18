e52a50cf3b8aca869f887a988a76dd5a6a7329ae
Farnoosh-Starr <farnoosh@stanford.edu>
2025-02-18 21:29:10 +0000

    modified the queries sourcing the yaml and flags

--- src/sql/tumor_board/tumor_board_omop_chemo_person_metrics.sql
+++ src/sql/tumor_board/tumor_board_omop_chemo_person_metrics.sql
@@ -1,12 +1,12 @@
+-------------------------------------------------------------------------
+-- this query generates tb patients who had chemo from arpah-cohort --
+------------------------------------------------------------------------
 with
-person as (select * from `som-rit-phi-oncology-prod.oncology_omop_arpah_alpha.person`),
-pat_enc as (select * from `som-rit-phi-starr-prod.shc_clarity_filtered_latest.pat_enc`),
-zc_disp_enc_type as (select * from `som-rit-phi-starr-prod.shc_clarity_filtered_latest.zc_disp_enc_type`),
-clarity_prc as (select * from `som-rit-phi-starr-prod.shc_clarity_filtered_latest.clarity_prc`),
-patient as (select * from `som-rit-phi-starr-prod.shc_clarity_filtered_latest.patient`),
-concept as (select * from `som-rit-phi-oncology-prod.oncology_omop_arpah_alpha.concept`),
-concept_ancestor as (select * from `som-rit-phi-oncology-prod.oncology_omop_arpah_alpha.concept_ancestor`),
-drug_era as (select * from `som-rit-phi-oncology-prod.oncology_omop_arpah_alpha.drug_era`),
+person as (select * from `@oncology_prod.@oncology_omop.person`),
+all_flag as (select * from `@oncology_dev.@oncology_temp.onc_all__cancer_flags`),
+concept as (select * from `@oncology_prod.@oncology_omop.concept`),
+concept_ancestor as (select * from `@oncology_prod.@oncology_omop.concept_ancestor`),
+drug_era as (select * from `@oncology_prod.@oncology_omop.drug_era`),
 chemo_med as
 (
   select distinct c.*
@@ -15,26 +15,13 @@ chemo_med as
     and ca.ancestor_concept_id in (21601386,724009)
     and c.invalid_reason is null
 ),
-tumor_board_patients AS (
-SELECT
-  DISTINCT p.pat_mrn_id,
-  extract(date from p.birth_date) birth_date
-FROM
-  pat_enc enc -- this is only getting tumor board patients from shc, not lpch, is that an issue?
-  left join zc_disp_enc_type et on enc.enc_type_c=et.disp_enc_type_c
-  left join clarity_prc on enc.appt_prc_id = clarity_prc.prc_id
-  inner join patient p on enc.pat_id=p.pat_id
-  where
-    (appt_status_c IS NULL OR appt_status_c = 2) --encounter is not marked as cancelled
-    and (et.name IS NULL or et.name != 'Erroneous Encounter') --encounter is not labeled as erroneous
-  and (
-    UPPER(clarity_prc.prc_name) IN ("DISCUSSION ONLY TUMOR BOARD", "IN PERSON TUMOR BOARD", "TUMOR BOARD", "TUMOR BOARD LIVER") -- visit type (aka appt_prc_id) of tumor board
-    or et.name = 'Tumor Board' --enc_type of tumor board
-  )
+tumor_board_patients 
+AS ( select person_source_value from all_flag
+where tumor_board_encounter_flag = 1
 )
 select count(distinct person.person_source_value) patient_count
 from
 tumor_board_patients tb
-inner join person on person.person_source_value = CONCAT(FORMAT('%s | %s', tb.pat_mrn_id, CAST(cast(tb.birth_date AS date) AS string format 'yyyy-mm-dd')))
+inner join person on person.person_source_value = tb.person_source_value
 inner join drug_era on person.person_id = drug_era.person_id
 inner join chemo_med on chemo_med.concept_id = drug_era.drug_concept_id
--- src/sql/tumor_board/tumor_board_person_metrics.sql
+++ src/sql/tumor_board/tumor_board_person_metrics.sql
@@ -1,27 +1,15 @@
+-------------------------------
+--- number of tb patients 
+-------------------------------
+
 with
-person as (select * from `som-rit-phi-oncology-prod.oncology_omop_arpah_alpha.person`),
-pat_enc as (select * from `som-rit-phi-starr-prod.shc_clarity_filtered_latest.pat_enc`),
-zc_disp_enc_type as (select * from `som-rit-phi-starr-prod.shc_clarity_filtered_latest.zc_disp_enc_type`),
-clarity_prc as (select * from `som-rit-phi-starr-prod.shc_clarity_filtered_latest.clarity_prc`),
-patient as (select * from `som-rit-phi-starr-prod.shc_clarity_filtered_latest.patient`),
-tumor_board_patients AS (
-SELECT
-  DISTINCT p.pat_mrn_id,
-  extract(date from p.birth_date) birth_date
-FROM
-  pat_enc enc -- this is only getting tumor board patients from shc, not lpch, is that an issue?
-  left join zc_disp_enc_type et on enc.enc_type_c=et.disp_enc_type_c
-  left join clarity_prc on enc.appt_prc_id = clarity_prc.prc_id
-  inner join patient p on enc.pat_id=p.pat_id
-  where
-    (appt_status_c IS NULL OR appt_status_c = 2) --encounter is not marked as cancelled
-    and (et.name IS NULL or et.name != 'Erroneous Encounter') --encounter is not labeled as erroneous
-  and (
-    UPPER(clarity_prc.prc_name) IN ("DISCUSSION ONLY TUMOR BOARD", "IN PERSON TUMOR BOARD", "TUMOR BOARD", "TUMOR BOARD LIVER") -- visit type (aka appt_prc_id) of tumor board
-    or et.name = 'Tumor Board' --enc_type of tumor board
-  )
+person as (select * from `@oncology_prod.@oncology_omop.person`),
+all_flag as (select * from `@oncology_dev.@oncology_temp.onc_all__cancer_flags`),
+tumor_board_patients 
+AS ( select person_source_value from all_flag
+where tumor_board_encounter_flag = 1
 )
 select count(distinct person.person_source_value) patient_count
 from
 tumor_board_patients tb
-inner join person on person.person_source_value = CONCAT(FORMAT('%s | %s', tb.pat_mrn_id, CAST(cast(tb.birth_date AS date) AS string format 'yyyy-mm-dd')))
+inner join person on person.person_source_value = tb.person_source_value
