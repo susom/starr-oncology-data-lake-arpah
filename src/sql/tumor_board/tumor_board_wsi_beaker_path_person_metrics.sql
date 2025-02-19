852828bf14897c36946ceaac68c815f6af838dfc
Farnoosh-Starr <farnoosh@stanford.edu>
2025-02-19 22:27:42 +0000

    deleted files

--- src/sql/tumor_board/tumor_board_wsi_beaker_path_person_metrics.sql
+++ src/sql/tumor_board/tumor_board_wsi_beaker_path_person_metrics.sql
@@ -3,7 +3,7 @@
 ----------------------------------------------
 with
 person as (select * from `@oncology_prod.@oncology_omop.person`),
-all_flag as (select * from `@oncology_dev.@oncology_tmp.onc_all__cancer_flags`),
+all_flag as (select * from `@oncology_dev.@oncology_temp.onc_all__cancer_flags`),
 patient as (select * from `@starr_prod.@shc_clarity.patient`),
 shc_pathology as (select * from `@starr_prod.@starr_common.shc_pathology`),
 wsi_beaker_path_patients as (
@@ -20,7 +20,7 @@ and sp.order_type = 'Pathology'
 and sp.acc_num like 'SP%'
 ),
 tumor_board_patients AS (
-SELECT
+SELECT distinct
  person_source_value from all_flag
 where tumor_board_encounter_flag = 1
 )
@@ -28,4 +28,5 @@ select count(distinct person.person_source_value) patient_count
 from
 tumor_board_patients tb
 inner join person on person.person_source_value = tb.person_source_value
-inner join wsi_beaker_path_patients wsi on person.person_source_value = CONCAT(FORMAT('%s | %s', wsi.pat_mrn_id, CAST(cast(wsi.birth_date AS date) AS string format 'yyyy-mm-dd')))
+inner join wsi_beaker_path_patients wsi on
+ person.person_source_value = CONCAT(FORMAT('%s | %s', wsi.pat_mrn_id, CAST(cast(wsi.birth_date AS date) AS string format 'yyyy-mm-dd')))
