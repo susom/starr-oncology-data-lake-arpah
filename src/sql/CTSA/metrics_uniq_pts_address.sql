48e0bf3c16a16349ab48e1843a2af1fc3e31e305
Farnoosh-Starr <farnoosh@stanford.edu>
2025-02-21 03:04:47 +0000

    added ctsa metric table

--- src/sql/CTSA/metrics_uniq_pts_address.sql
+++ src/sql/CTSA/metrics_uniq_pts_address.sql
@@ -6,4 +6,4 @@ select ps.person_id, loc.* from  `@oncology_prod.@oncology_omop.person` ps
 left join `@oncology_prod.@oncology_omop.location` loc
 on ps.location_id=loc.location_id
 where address_1 is not null or address_2 is not null
-) select count(distinct person_id) as uniq_pts_address from address
+) select count(distinct person_id) as uniq_pts_address , 'uniq_pt_address' as variable_name from address
