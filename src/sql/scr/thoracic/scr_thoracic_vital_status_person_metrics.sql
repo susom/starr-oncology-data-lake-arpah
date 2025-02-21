0bf02c6474f75b41eac9576198e79d6e7135c41a
solomonahenry <shenry@stanford.edu>
2025-02-21 23:22:24 +0000

    Used the onc_all__cancer_flags table and added parameterization

--- src/sql/scr/thoracic/scr_thoracic_vital_status_person_metrics.sql
+++ src/sql/scr/thoracic/scr_thoracic_vital_status_person_metrics.sql
@@ -4,7 +4,6 @@
 with
 scr as (select * from `@oncology_dev.@oncology_common.onc_neuralframe_case_diagnoses`),
 person as (select * from `@oncology_prod.@oncology_omop.person`),
-death as (select * from `@oncology_prod.@oncology_omop.death`),
 all_flag as (select * from `@oncology_dev.@oncology_temp.onc_all__cancer_flags`),
 death_src as (select distinct person_source_value from all_flag where scr_death_date is not null
 or death_datetime is not null),
@@ -23,7 +22,4 @@ count(distinct person.person_source_value) patient_count
   from
   person
 inner join scr_thoracic_patients on person.person_source_value = concat(scr_thoracic_patients.cleaned_nf_mrn, ' | ', scr_thoracic_patients.cleaned_nf_dob)
-left join death on person.person_id = death.person_id
-left join death_src on person.person_source_value=death_src.person_source_value
-where death.death_date is not null or
-death_src.person_source_value is not null
+inner join death_src on person.person_source_value=death_src.person_source_value
