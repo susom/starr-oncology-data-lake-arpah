a901e635d4fbf830ed974f39d97e57c5d04272f3
solomonahenry <shenry@stanford.edu>
2025-02-21 21:03:23 +0000

    onc_all__cancer_flags table has death date for both OMOP and SCR. Removed the OMOP death table join from the SQL

--- src/sql/arpah_clinical/vital_status_person_metrics.sql
+++ src/sql/arpah_clinical/vital_status_person_metrics.sql
@@ -3,14 +3,12 @@
 with
 person as (select * from `@oncology_prod.@oncology_omop.person`),
 all_flag as (select * from `@oncology_dev.@oncology_temp.onc_all__cancer_flags`),
-death as (select * from `@oncology_prod.@oncology_omop.death`),
 death_src as (select distinct person_source_value from all_flag where scr_death_date is not null
 or death_datetime is not null)
 select
 count(distinct person.person_source_value) patient_count
   from
   person 
- left join death on person.person_id = death.person_id
  left join death_src on person.person_source_value=death_src.person_source_value
  where 
- death.death_date is not null or death_src.person_source_value is not null
+ death_src.person_source_value is not null
