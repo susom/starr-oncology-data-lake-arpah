262e8f5d7697207048fd5e5201e264628cc830bd
solomonahenry <shenry@stanford.edu>
2025-02-26 18:12:21 +0000

    The OMOP death table is used in addition to the death_datetime column in the onc_all__cancer_flags table.

--- src/sql/arpah_clinical/vital_status_person_metrics.sql
+++ src/sql/arpah_clinical/vital_status_person_metrics.sql
@@ -3,10 +3,15 @@
 with
 person as (select * from `@oncology_prod.@oncology_omop.person`),
 all_flag as (select * from `@oncology_dev.@oncology_temp.onc_all__cancer_flags`),
+death as (select * from `@oncology_prod.@oncology_omop.death`),
 death_src as (select distinct person_source_value from all_flag where scr_death_date is not null
 or death_datetime is not null)
 select
 count(distinct person.person_source_value) patient_count
   from
   person 
- inner join death_src on person.person_source_value=death_src.person_source_value
+left join death_src on person.person_source_value=death_src.person_source_value
+left join death on person.person_id = death.person_id
+where
+death_src.person_source_value is not null
+or death.death_date is not null
