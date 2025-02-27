b522964eeefda58cae97481215ce3dc259c67b7f
Farnoosh-Starr <farnoosh@stanford.edu>
2025-02-27 17:11:05 +0000

    nf tables

--- src/sql/neuralframe/nf_vital_status.sql
+++ src/sql/neuralframe/nf_vital_status.sql
@@ -1,11 +1,12 @@
 
 ----------------------
---- cancer status 
+--- vital status 
 ------------------------
 with nf as (
 select scr.*  from `som-rit-phi-oncology-prod.oncology_omop_arpah_alpha.person` p
 inner join som-rit-phi-oncology-prod.oncology_neuralframe_arpah_alpha.onc_neuralframe_case_outcomes scr
 on p.person_source_value = concat(scr.cleaned_nf_mrn, ' | ', scr.cleaned_nf_dob)
-) select cancerstatusdescription, COUNT(DISTINCT cleaned_nf_mrn) as n_pts  from nf 
+) select vitalstatusdescription, COUNT(DISTINCT cleaned_nf_mrn) as n_pts from nf 
 group by 1
 order by 2 desc
+
