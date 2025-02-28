1ef2fbc833674b9ec451f5eeed85bacbc8c74da9
Farnoosh-Starr <farnoosh@stanford.edu>
2025-02-28 01:06:41 +0000

    update the sql for pathology

--- src/sql/arpah_clinical/wsi_beaker_path_person_metrics.sql
+++ src/sql/arpah_clinical/wsi_beaker_path_person_metrics.sql
@@ -1,22 +1,6 @@
 with
 person as (select * from `@oncology_prod.@oncology_omop.person`),
-shc_pathology as (select * from `@starr_prod.@starr_common.shc_pathology`),
-patient as (select * from `@starr_prod.@shc_clarity.patient`),
-wsi_beaker_path_patients as (
-select
-distinct
-p.pat_mrn_id,
-p.birth_date
-from
-shc_pathology sp
-join patient p on p.pat_id = sp.pat_id
-where
-sp.report_source = 'Beaker AP'
-and sp.order_type = 'Pathology'
-and sp.acc_num like 'SP%'
-)
-select
-  count(distinct person.person_source_value) patient_count
-  from
-  wsi_beaker_path_patients p
-inner join person on person.person_source_value = CONCAT(FORMAT('%s | %s', p.pat_mrn_id, CAST(cast(p.birth_date AS date) AS string format 'yyyy-mm-dd')))
+all_flag as (select * from `@oncology_prod.@oncology_temp.onc_arpah__cancer_cohort`)
+select count(distinct person_source_value) from all_flag 
+inner join person on person.person_source_value = all_flag.person_source_value
+where  (pts_pathology_flag=1)
