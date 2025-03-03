8f82e55d7f1f0d346d98b435de8b9248124c0d12
Farnoosh-Starr <farnoosh@stanford.edu>
2025-03-03 17:05:36 +0000

    pathology added

--- src/sql/arpah_clinical/wsi_beaker_path_person_metrics.sql
+++ src/sql/arpah_clinical/wsi_beaker_path_person_metrics.sql
@@ -1,6 +1,6 @@
 with
 person as (select * from `@oncology_prod.@oncology_omop.person`),
-all_flag as (select * from `@oncology_prod.@oncology_temp.onc_arpah__cancer_cohort_v2_v2`)
+all_flag as (select * from `@oncology_prod.@oncology_temp.onc_arpah__cancer_cohort_v2`)
 select count(distinct person.person_source_value) patient_count from all_flag 
 inner join person on person.person_source_value = all_flag.person_source_value
 where  (pts_pathology_flag=1)
