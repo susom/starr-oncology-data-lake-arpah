26d19cca0a4ec6daf69de01e383f889f05ef126a
Farnoosh-Starr <farnoosh@stanford.edu>
2025-03-06 21:59:40 +0000

    adding queries for image occu

---
+++ src/sql/image_occ/image_occ_person.sql
@@ -0,0 +1,15 @@
+
+--------------------------------------------------------
+--- number of unique series and number of unique pts ---
+--------------------------------------------------------
+
+SELECT COUNT(*) AS counts, 'Number of series in image_occurrence table' AS variable_name
+FROM (
+    SELECT DISTINCT image_study_uid, image_series_uid
+    FROM `@oncology_prod.@oncology_omop.image_occurrence`
+) AS distinct_series
+
+UNION ALL
+
+SELECT COUNT(DISTINCT person_id) AS counts, 'Number of unique persons in image_occurrence table' AS variable_name
+FROM `@oncology_prod.@oncology_omop.image_occurrence`;
