eb0e81cce28110721de70260e6c4911d4a37198e
Farnoosh-Starr <farnoosh@stanford.edu>
2025-04-22 00:39:18 +0000

    nf case status metrics

---
+++ src/sql/scr/thoracic/scr_case_status.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------------------------
+-- Number of cases by nfcasestatus (Completed, Not Reportable,Other)
+---------------------------------------------------------------------------
+
+with
+person as (select * from `@oncology_prod.@oncology_omop.person`),
+scr as (select * from `@oncology_prod.@oncology_neuralframe.onc_neuralframe_case_diagnoses`)
+select nfcasestatus 
+, count(distinct person_id) as unique_person_count_case from scr
+inner join person p on p.person_source_value = concat(scr.cleaned_nf_mrn, ' | ', scr.cleaned_nf_dob)
+group by 1 
+order by 2 desc
---
+++ src/sql/scr/thoracic/scr_thoracic_denominator.sql
@@ -0,0 +1,36 @@
+
+--------------------------------------------------------------------------
+-- Number of thoracic cancer patients in the denominator
+---------------------------------------------------------------------------
+
+with 
+person as (select * from `@oncology_prod.@oncology_omop.person`),
+scr as (select * from  `@oncology_prod.@oncology_neuralframe.onc_neuralframe_case_diagnoses`),
+scr_data as
+(
+select
+distinct cleaned_nf_mrn, cleaned_nf_dob,
+primarySiteDescription,nfcasestatus
+from
+scr nf
+where
+trim(medicalRecordNumber) <> ''and length(dateOfBirth) = 8
+),
+scr_omop as
+(select
+ distinct
+ p.person_id,
+ p.person_source_value,
+ primarySiteDescription,
+ nfcasestatus
+from
+scr_data
+inner join person p on p.person_source_value = concat(scr_data.cleaned_nf_mrn, ' | ', scr_data.cleaned_nf_dob)
+)
+select
+count(distinct person_source_value) as unique_thoracic_cancer_pts
+from scr_omop
+where
+lower(primarysiteDescription) like '%lung%'
+or lower(primarysiteDescription) like '%bronchus%'
+or lower(primarysiteDescription) like '%thymus%'
