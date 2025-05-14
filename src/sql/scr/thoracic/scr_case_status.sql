25f481cd08076f7bda25a466cb3a10ee2710b8b1
Farnoosh-Starr <farnoosh@stanford.edu>
2025-05-14 20:54:02 +0000

    nf final

--- src/sql/scr/thoracic/scr_case_status.sql
+++ src/sql/scr/thoracic/scr_case_status.sql
@@ -6,7 +6,7 @@ with
 person as (select * from `@oncology_prod.@oncology_omop.person`),
 scr as (select * from `@oncology_prod.@oncology_neuralframe.onc_neuralframe_case_diagnoses`)
 select nfcasestatus 
-, count(distinct person_id) as unique_person_count_case from scr
+, count(distinct p.person_id) as unique_person_count_case from scr
 inner join person p on p.person_source_value = scr.stanford_patient_uid
 group by 1 
 order by 2 desc
--- src/sql/scr/thoracic/scr_den.sql
+++ src/sql/scr/thoracic/scr_den.sql
@@ -5,5 +5,5 @@
 with
 person as (select * from `@oncology_prod.@oncology_omop.person`),
 scr as (select * from `@oncology_prod.@oncology_neuralframe.onc_neuralframe_case_diagnoses`)
-select count(distinct person_id) as unique_person_count from scr
+select count(distinct p.person_id) as unique_person_count from scr
 inner join person p on p.person_source_value = scr.stanford_patient_uid
