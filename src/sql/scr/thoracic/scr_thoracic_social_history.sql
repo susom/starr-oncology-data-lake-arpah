3fbfe090c967d3262db022c55eb52f6c2f87bc53
Farnoosh-Starr <farnoosh@stanford.edu>
2025-05-14 20:54:02 +0000

    nf final

--- src/sql/scr/thoracic/scr_thoracic_social_history.sql
+++ src/sql/scr/thoracic/scr_thoracic_social_history.sql
@@ -10,20 +10,18 @@ where
 lower(primarysiteDescription) like '%lung%'
 or lower(primarysiteDescription) like '%bronchus%'
 or lower(primarysiteDescription) like '%thymus%'
-and nfcasestatus="Completed" ),
+),
 scr_data as
 (select scr.* from scr
 inner join person p on p.person_source_value = scr.stanford_patient_uid
 inner join dx on scr.stanford_patient_uid=dx.stanford_patient_uid
 ),
-smk_stat as (select naaccrtobaccodescription, count(*) as counts, 'smoking status' as flag
+smk_stat as (select naaccrtobaccodescription, count(distinct(stanford_patient_uid)) as counts, 'smoking status' as flag
 from scr_data
-where naaccrtobaccodescription is not null
 group by 1
 order by 1),
-alc_stat as (select naaccralcoholdescription, count(*) as counts, 'alcohol status' as flag
+alc_stat as (select naaccralcoholdescription, count(distinct(stanford_patient_uid)) as counts, 'alcohol status' as flag
 from scr_data
-where naaccralcoholdescription is not null
 group by 1
 order by 1)
 select * from smk_stat union all select * from alc_stat
