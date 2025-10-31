4113d9ff2b740137eea95a56f8518b032206f5dd
Farnoosh-Starr <farnoosh@stanford.edu>
2025-10-31 15:00:02 +0000

    exclude queries

--- src/nov_2025/sql/arpah_clinical/note_path_cyto_person_metrics.sql
+++ src/nov_2025/sql/arpah_clinical/note_path_cyto_person_metrics.sql
@@ -1,10 +1,13 @@
 with
-person as (select * from `@oncology_prod.@oncology_omop.person`),
-note as (select * from `@oncology_prod.@oncology_omop.note`)
+person as (select * from `@oncology_omop.person`),
+note as (select * from `@oncology_omop.note`)
 select
-count(distinct person.person_source_value) patient_count
+count(distinct person.person_source_value) as patient_count,
+count(distinct note.note_id) as pathology_report_count
 from
 note
 join person on note.person_id = person.person_id
 where
 note_title in ('pathology','pathology and cytology')
+
+
