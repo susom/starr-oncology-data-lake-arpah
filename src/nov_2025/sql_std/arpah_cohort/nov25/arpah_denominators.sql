360452fe305f71751188bd121eed210ae61380b7
Farnoosh-Starr <farnoosh@stanford.edu>
2025-11-12 20:26:09 +0000

    arpah cohort

---
+++ src/nov_2025/sql_std/arpah_cohort/nov25/arpah_denominators.sql
@@ -0,0 +1,30 @@
+
+WITH oncology_count AS (
+    SELECT COUNT(DISTINCT person_id) AS counts_pts
+    FROM `@oncology_omop.person`
+)
+SELECT 
+    counts_pts, 
+    data_set, 
+    (counts_pts * 100.0 / (SELECT counts_pts FROM oncology_count)) AS percentage
+FROM (
+    SELECT COUNT(DISTINCT person_id) AS counts_pts, 'Oncology OMOP (Cohort)' AS data_set 
+    FROM `@oncology_omop.person`
+    
+    UNION ALL 
+    
+    SELECT COUNT(DISTINCT person_id) AS counts_pts, 'With Image Occurrence' AS data_set 
+    FROM `@oncology_omop.image_occurrence`
+    
+    UNION ALL
+    
+    SELECT COUNT(DISTINCT stanford_patient_uid) AS counts_pts, 'Neural Frame' AS data_set 
+    FROM `@oncology_omop.person` p
+    INNER JOIN `@oncology_neuralframe.onc_neuralframe_case_outcomes` nf
+    ON p.person_source_value = nf.stanford_patient_uid
+
+    UNION ALL
+    SELECT COUNT(DISTINCT person_id) AS counts_pts, 'With Tumor Board Encounter' AS data_set
+    FROM `@oncology_omop.visit_occurrence` 
+    WHERE  LOWER(visit_source_value) LIKE '%tumor board%'
+) AS counts 
