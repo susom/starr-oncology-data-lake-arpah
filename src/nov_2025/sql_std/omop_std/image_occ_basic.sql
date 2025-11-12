f4d317455a7a1358d70f99340666df56ad8b903c
Farnoosh-Starr <farnoosh@stanford.edu>
2025-11-12 19:40:26 +0000

    std omop

---
+++ src/nov_2025/sql_std/omop_std/image_occ_basic.sql
@@ -0,0 +1,18 @@
+
+----------------------------------
+--- Basic Counts and Data Size  ---
+-----------------------------------
+SELECT count(distinct(modality_source_value)) as counts, 'counts_modality_type' as flag FROM  `.@oncology_omop.image_occurrence` 
+union all 
+SELECT count(distinct(anatomic_site_source_value)) as counts, 'counts_anatomic_type' as flag FROM `.@oncology_omop.image_occurrence` 
+union all
+SELECT
+  ROUND(SUM(size_bytes)/1024/1024/1024) AS counts,  'size_gb' as flag
+FROM
+  `@oncology_omop.__TABLES__`
+WHERE
+  table_id = 'image_occurrence'
+  union all
+select count(distinct(image_study_uid)) as counts, 'counts_study' as flag from `.@oncology_omop.image_occurrence`
+union all
+select count(distinct(image_series_uid)) as counts, 'counts_series' as flag from `.@oncology_omop.image_occurrence`
