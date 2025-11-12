c99785271abe1953b769ca8c31602c2234f3312d
Farnoosh-Starr <farnoosh@stanford.edu>
2025-11-12 19:45:39 +0000

    std philips

--- src/nov_2025/sql_std/philips_std/nov25/philips_denominator.sql
+++ src/nov_2025/sql_std/philips_std/nov25/philips_denominator.sql
@@ -6,4 +6,4 @@
     'Philips ISPM' AS data_set 
     FROM `@oncology_prod.@oncology_omop.person` p
     INNER JOIN `@oncology_prod.@oncology_philips.onc_philips_mtb_pat_diag_orders` phi
-    ON p.person_source_value = phi.stanford_patient_uid
+    ON json_value(p.person_source_value, '.$stanford_patient_uid') = phi.stanford_patient_uid
--- src/nov_2025/sql_std/philips_std/nov25/philips_tests_date_ordered.sql
+++ src/nov_2025/sql_std/philips_std/nov25/philips_tests_date_ordered.sql
@@ -6,7 +6,7 @@
     select phi.*
     FROM `@oncology_prod.@oncology_omop.person` p
     INNER JOIN `@oncology_prod.@oncology_philips.onc_philips_mtb_pat_diag_orders` phi
-    ON p.person_source_value = phi.stanford_patient_uid
+    ON json_value(p.person_source_value, '.$stanford_patient_uid') = phi.stanford_patient_uid
    )
     select test_type, 
             min(date_of_test) as min_order_date,
