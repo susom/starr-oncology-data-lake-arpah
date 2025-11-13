1047dfbaddb22f9654f43d51c55684f5a61d4a06
Farnoosh-Starr <farnoosh@stanford.edu>
2025-11-13 17:09:58 +0000

    metrics clinical

--- src/nov_2025/sql_std/arpah_cohort/nov25/arpah_overlap_metrics_updated.sql
+++ src/nov_2025/sql_std/arpah_cohort/nov25/arpah_overlap_metrics_updated.sql
@@ -15,18 +15,16 @@
     SELECT DISTINCT p.person_id, 'Neural Frame' AS data_set 
     FROM @oncology_prod.@oncology_omop.person p
     INNER JOIN @oncology_prod.@oncology_neuralframe.onc_neuralframe_case_outcomes nf
-    ON p.person_source_value = nf.stanford_patient_uid
+    ON json_value(p.person_source_value, '$.stanford_patient_uid') = nf.stanford_patient_uid
     
     UNION ALL 
     
     SELECT DISTINCT p.person_id, 'Philips ISPM' AS data_set 
     FROM @oncology_prod.@oncology_omop.person p
     INNER JOIN @oncology_prod.@oncology_philips.onc_philips_mtb_pat_diag_orders phi
-    ON p.person_source_value = phi.stanford_patient_uid
+    ON json_value(p.person_source_value, '$.stanford_patient_uid') = phi.stanford_patient_uid
 
     UNION ALL
-    SELECT DISTINCT p.person_id, 'Tumor Board' AS data_set
-    FROM @oncology_prod.@oncology_omop.person p
-    inner join @oncology_prod.@oncology_temp.onc_arpah__cancer_cohort tb
-    on p.person_source_value = tb.person_source_value
-    where tumor_board_encounter_flag = 1
+    SELECT DISTINCT person_id, 'Tumor Board' AS data_set
+    FROM @oncology_prod.@oncology_omop.visit_occurrence
+    where LOWER(visit_source_value) LIKE '%tumor board%'
