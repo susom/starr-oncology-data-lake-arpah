b80a0e22d5b9b2738a47eebfac74fcd10550d235
Farnoosh-Starr <farnoosh@stanford.edu>
2025-11-26 20:46:19 +0000

    removed anatomic

--- src/nov_2025/sql_std/omop/image_occ/nov25/image_occ_basic.sql
+++ src/nov_2025/sql_std/omop/image_occ/nov25/image_occ_basic.sql
@@ -4,15 +4,14 @@
 -----------------------------------
 SELECT count(distinct(modality_source_value)) as counts, 'counts_modality_type' as flag FROM  `@oncology_prod.@oncology_omop.image_occurrence` 
 union all 
-SELECT count(distinct(anatomic_site_source_value)) as counts, 'counts_anatomic_type' as flag FROM `@oncology_prod.@oncology_omop.image_occurrence` 
-union all
-SELECT
-  ROUND(SUM(size_bytes)/1024/1024/1024) AS counts,  'size_gb' as flag
-FROM
-  `@oncology_prod.@oncology_omop.__TABLES__`
-WHERE
-  table_id = 'image_occurrence'
-  union all
+--SELECT count(distinct(anatomic_site_source_value)) as counts, 'counts_anatomic_type' as flag FROM `@oncology_prod.@oncology_omop.image_occurrence` 
+  --union all
 select count(distinct(image_study_uid)) as counts, 'counts_study' as flag from `@oncology_prod.@oncology_omop.image_occurrence`
 union all
 select count(distinct(image_series_uid)) as counts, 'counts_series' as flag from `@oncology_prod.@oncology_omop.image_occurrence`
+union all
+select count(distinct(visit_occurrence_id)) as counts, 'counts_visit_occ' as flag from `@oncology_prod.@oncology_omop.image_occurrence`
+union all
+select count(distinct(_note_id)) as counts, 'counts_note_id' as flag from `@oncology_prod.@oncology_omop.image_occurrence`
+union all
+select count(distinct(_accession_number)) as counts, 'counts_accession_number' as flag from `@oncology_prod.@oncology_omop.image_occurrence`
