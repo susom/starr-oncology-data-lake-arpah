aaf45a9b22030bea8290fcee0236b739cb320f1d
Farnoosh-Starr <farnoosh@stanford.edu>
2026-02-22 18:19:53 +0000

    Feb2026 updates

--- src/feb_2026/sql_std/omop/image_occ/may_aug25/image_occ_basic.sql
+++ src/feb_2026/sql_std/omop/image_occ/may_aug25/image_occ_basic.sql
@@ -4,8 +4,8 @@
 -----------------------------------
 SELECT count(distinct(modality_source_value)) as counts, 'counts_modality_type' as flag FROM  `@oncology_prod.@oncology_omop.image_occurrence` 
 union all 
---SELECT count(distinct(anatomic_site_source_value)) as counts, 'counts_anatomic_type' as flag FROM `@oncology_prod.@oncology_omop.image_occurrence` 
-  --union all
+SELECT count(distinct(anatomic_site_source_value)) as counts, 'counts_anatomic_type' as flag FROM `@oncology_prod.@oncology_omop.image_occurrence` 
+  union all
 select count(distinct(image_study_uid)) as counts, 'counts_study' as flag from `@oncology_prod.@oncology_omop.image_occurrence`
 union all
 select count(distinct(image_series_uid)) as counts, 'counts_series' as flag from `@oncology_prod.@oncology_omop.image_occurrence`
