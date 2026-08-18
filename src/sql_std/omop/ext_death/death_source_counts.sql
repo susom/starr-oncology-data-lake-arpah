ae08a8cb35ee484102d9c6eab1cd0f374f1a78cd
Farnoosh-Starr <farnoosh@stanford.edu>
2026-08-18 22:36:54 +0000

    death table makdown

--- src/sql_std/omop/ext_death/death_source_counts.sql
+++ src/sql_std/omop/ext_death/death_source_counts.sql
@@ -20,8 +20,7 @@ ext_death AS (
 all_death_combined AS (
   SELECT person_id FROM omop_death
   UNION DISTINCT
-  SELECT person_id FROM neuralframe_death
-  UNION DISTINCT
+  --SELECT person_id FROM neuralframe_death
   SELECT person_id FROM ext_death
 )
 
@@ -39,10 +38,8 @@ FROM omop_death
 
 UNION ALL
 
-SELECT 'NeuralFrame death' AS death_source, COUNT(DISTINCT person_id) AS n_unique_patients
-FROM neuralframe_death
-
-UNION ALL
+--SELECT 'NeuralFrame death' AS death_source, COUNT(DISTINCT person_id) AS n_unique_patients
+--FROM neuralframe_death
 
 SELECT 'External death records' AS death_source, COUNT(DISTINCT person_id) AS n_unique_patients
 FROM ext_death
